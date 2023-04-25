c45087a36f049aaad72ba2e5bf81dd5dc6f11a77
Moisés Aguirre <121882599+maguirre42@users.noreply.github.com>
2023-04-25 15:17:06 +0200

    Add files via upload

---
+++ ClaseArrays/ClaseArrays/src/OrdenDescendente.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class OrdenDescendente {
+    public static void main(String[] args) {
+
+        int[] numeros = {2,7,9,3,1,5};
+        ordenInverso(numeros);
+        for(int i:numeros){
+            System.out.println("i = " + i);
+        }
+
+        String[] col = {"azul","rojo","verde","amarillo","naranja"};
+        ordenInverso(col);
+        for(String color:col){
+            System.out.println("color = " + color);
+        }
+    }
+
+    // Ordenar un array numérico en orden descendente
+    public static void ordenInverso(int[] numeros){
+        Arrays.sort(numeros);
+        int[] aux = Arrays.copyOf(numeros,numeros.length);
+        for (int i = 0; i < numeros.length; i++){
+            int indice = numeros.length - i - 1;
+            numeros[i] = aux[indice];
+        }
+    }
+
+    public static void ordenInverso(String[] colores){
+        Arrays.sort(colores);
+        String[] aux = Arrays.copyOf(colores,colores.length);
+        for(int i = 0; i < colores.length; i++){
+            colores[i] = aux[colores.length - i - 1];
+        }
+    }
+
+
+}

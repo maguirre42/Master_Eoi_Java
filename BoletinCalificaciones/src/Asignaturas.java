2f9f03d35becaa88d8ead65b267aef7706317a26
Moisés Aguirre <121882599+maguirre42@users.noreply.github.com>
2023-04-28 16:54:02 +0200

    Add ejercicio final Java Básico
    Crear
    
    Un programa que crea un boletin de notas introduciendo las diferentes notas en cada asignatura

---
+++ BoletinCalificaciones/src/Asignaturas.java
@@ -0,0 +1,22 @@
+public enum Asignaturas {
+    MATEMATICAS(25,"Matematicas"),
+    FISICA(43,"Fisica  "),
+    QUIMICA(44,"Quimica  "),
+    FILOSOFIA(67,"Filosofia"),
+    HISTORIA(80,"Historia");
+
+    // Atributos
+    private final int codigo;
+    private final String nombreAsignatura;
+
+    // Constructor
+    Asignaturas(int codigo, String nombreAsignatura) {
+        this.codigo = codigo;
+        this.nombreAsignatura = nombreAsignatura;
+    }
+
+    // Getters y setters
+    public String getNombreAsignatura() {
+        return nombreAsignatura;
+    }
+}

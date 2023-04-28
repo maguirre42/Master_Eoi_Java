8a76bc1c36ca99f177b71690198d174c5a1c8415
Moisés Aguirre <121882599+maguirre42@users.noreply.github.com>
2023-04-28 17:21:38 +0200

    Update Amigos.java

--- POO2/POO2/src/Amigos.java
+++ POO2/POO2/src/Amigos.java
@@ -1,13 +1,14 @@
 public enum Amigos {
+    // Declarar elementos
     PEPE("José Marín","629558897","pepe@eoi.es"),
     MARTA("Marta Pérez","629555552","marta@eoi.es"),
     MARIA("María Corral","629555553","maria@eoi.es"),
     ANTONIO("Antonio Cnales","629555554","antonio@eoi.es");
-
+    // Declarar atributos
     private final String nombre;
     private final String telefono;
     private final String mail;
-
+    // Constructores
     Amigos(String nombre, String telefono, String mail) {
         this.nombre = nombre;
         this.telefono = telefono;

cd2aa4d49dd83ff1668eb16458af1dc8e76d84ef
Moisés Aguirre <121882599+maguirre42@users.noreply.github.com>
2023-04-28 16:42:33 +0200

    Update EjemploThrow.java

--- Excepciones/Excepciones/src/EjemploThrow.java
+++ Excepciones/Excepciones/src/EjemploThrow.java
@@ -1,12 +1,13 @@
 public class EjemploThrow {
     public static void main(String[] args) {
+    // Try...catch 
         try {
             comprobarEdad(17);
         }catch(ArithmeticException e){
             System.out.println("e.toString() = " + e.toString());
         }
     }
-
+    // Metodo para verificar si alguien tiene 18 años o mas
     static void comprobarEdad(int edad){
         if(edad < 18){
             //System.out.println("Acceso denegado");
